f58c7af29eefa56506f735f95b0fd675d123274f
Edwin Aritama <aritamaedwin@gmail.com>
2025-05-31 19:36:26 +0700

    edit gambar

--- src/app/admin/product/view/[viewId]/ViewPage.jsx
+++ src/app/admin/product/view/[viewId]/ViewPage.jsx
@@ -122,24 +122,26 @@ const ViewPage = () => {
           <hr />
 
           
-          <div className="border-1 border-gray-300 rounded-md px-3 py-3 flex flex-row gap-4">
+          <div className="border-1 border-gray-300 rounded-md px-3 py-3 flex flex-wrap gap-4 w-full">
             {product?.data?.Images && product?.data?.Images.length > 0 ? (
-              product?.data?.Images.map((image, index) => {
-                return (
-                  <div key={index} className="flex flex-col gap-3 ">
-                    <Image
-                      src={`http://localhost:4000${image.image}`}
-                      onClick={() => window.open(`http://localhost:4000${image.image}`)}
-                      alt={image.image}
-                      className="max-w-[200px] min-h-[200px] max-h-[200px] object-cover"
-                      width={200}
-                      height={200}
-                    />
-                  </div>
-                );
-              })
+              product?.data?.Images.map((image, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col gap-3 min-h-[220px] max-w-[200px] flex-grow"
+                  style={{ flex: "0 0 auto" }} // Ensures items maintain their size and wrap as needed
+                >
+                  <Image
+                    src={`http://localhost:4000${image.image}`}
+                    onClick={() => window.open(`http://localhost:4000${image.image}`)}
+                    alt={image.image}
+                    className="max-w-[200px] min-h-[200px] max-h-[200px] object-cover"
+                    width={200}
+                    height={200}
+                  />
+                </div>
+              ))
             ) : (
-              <div className="text-gray-500">Gambar tidak ditemukan</div>
+              <div className="text-gray-500 w-full text-center">Gambar tidak ditemukan</div>
             )}
           </div>
 
